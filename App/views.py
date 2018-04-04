8abea9313e86a6a7245e080323c3f81db77d21f6
Moh'd Salem <m_saady_salem@msn.com>
2018-04-04 09:31:14 +0000

    Add my_topics and topic_delete views

--- App/views.py
+++ App/views.py
@@ -6,25 +6,27 @@ import random
 from flask import (redirect,
                    render_template,
                    request,
-                   url_for)
+                   url_for,
+                   flash)
 
 from App import (app,
                  db_con,
                  models)
 
+app.config["SECRET_KEY"] = '\x87\x1f\x03\xa5\x90\x8c\xacC\x95\xcbB\xe4\xc8\xdb=\x80+\xc3\xd7\x8e\xad\x90\x8f9\x12\x81?\xd4\x82L\xcb\xc1'
+
 
 @app.route("/")
 @app.route("/index/")
 def home():
-    title = "OMAC - All Topics"
-    return render_template("index.html",
-                           title=title,
+    return render_template("topics/index.html",
+                           title="OMAC - Add Topic",
                            posts=db_con.post_store.get_all(),
                            members=db_con.member_store)
 
 
 @app.route("/topic/add", methods=["GET", "POST"])
-def add_topic():
+def topic_add():
     if request.method == "POST":
         if request.form["submit"] == "Add":
             new_post = models.Post(request.form["topic_title"],
@@ -35,5 +37,30 @@ def add_topic():
         else:
             return redirect(url_for("home"))
     else:
-        title = "OMAC - Add Topic"
-        return render_template("add_topic.html", title=title)
+        return render_template("topics/topic_add.html",
+                               title="OMAC - Add Topic")
+
+
+@app.route("/topic/delete/<int:id>", methods=["GET", "POST"])
+def topic_delete(id):
+    post = db_con.post_store.get_by_id(id)
+    if request.method == "POST":
+        member_id = post.member_id
+        if request.form['submit'] == "Delete":
+            db_con.post_store.delete(id)
+            db_con.update_members_posts()
+            member = db_con.member_store.get_by_id(member_id)
+            return redirect(url_for("my_topics", id=member.id))
+        else:
+            return redirect(url_for("my_topics", id=member_id))
+    return render_template("topics/topic_delete.html",
+                           title="OMAC - Delete Topic",
+                           post=post)
+
+
+@app.route("/my_topics/<int:id>")
+def my_topics(id):
+    member = db_con.member_store.get_by_id(id)
+    return render_template("topics/my_topics.html",
+                           title="OMAC - My Topics",
+                           member=member)
