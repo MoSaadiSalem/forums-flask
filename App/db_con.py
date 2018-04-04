d6dd2c0d560235943868444c62b5320001fa9a88
Moh'd Salem <m_saady_salem@msn.com>
2018-04-04 09:13:44 +0000

    Add posts to their members

--- App/db_con.py
+++ App/db_con.py
@@ -8,3 +8,10 @@ member_store = stores.MemberStore()
 post_store = stores.PostStore()
 
 dummy_data.seed_stores(member_store, post_store)
+
+
+def update_members_posts():
+    member_store.get_members_with_posts(post_store.posts)
+
+
+update_members_posts()
